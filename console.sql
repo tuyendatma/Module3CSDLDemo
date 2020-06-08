930f0347333e5b38560f8bfa98902cfa9d6b2a51
tuyendatma <62637890+tuyendatma@users.noreply.github.com>
2020-06-08 16:42:37 +0700

    CSDL demo

---
+++ console.sql
@@ -0,0 +1,4 @@
+use classicmodels;
+select * from employees;
+SELECT * FROM customers WHERE city IN ('Nantes',' Las Vegas',' Warszawa','NYC');
+SELECT * FROM customers WHERE customername like '%A%';
